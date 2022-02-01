2f498a0318796f1e8091f7dda5306c165f67c945
Vladimir Dubovik <borisonia0216@gmail.com>
2022-02-01 19:41:46 +0300

    сделал получение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 let parse = document.getElementById('parse'),
     show = document.getElementById('show'),
-    request = new XMLHttpRequest();
-    
-    
-request.open('GET', 'https://api.coingecko.com/api/v3/global');
-request.responseType = 'text';
-parse.onclick =  function() {
-    console.log('!')
+    requestURL = 'https://api.coingecko.com/api/v3/global',
+    request = new XMLHttpRequest(),
+    data;
+
+parse.onclick = function () {
+    request.open('GET', requestURL);
+    request.responseType = 'text';
+    request.send();
+    request.onload = function () {
+        data = request.response;
+        data = JSON.parse(data);
+    };
+    console.log(data);
 };
-show.onclick = function() {
+show.onclick = function () {
     alert('!');
 };
