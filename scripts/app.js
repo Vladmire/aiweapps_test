51b6b0556b7585a47caae75e02137902fed80751
Vladimir Dubovik <borisonia0216@gmail.com>
2022-02-02 21:48:06 +0300

    все в сборе

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,21 @@
 let parse = document.getElementById('parse'),
     show = document.getElementById('show'),
-    requestURL = 'https://api.coingecko.com/api/v3/global',
-    request = new XMLHttpRequest(),
-    data,
-    db = new Object(),
+    requestURL = 'https://api.coingecko.com/api/v3/global', //request to api
+    request = new XMLHttpRequest(), // using XMLHttp api for making request
+    data,              // data from api
+    db = new Object(), // create data base   
     criptoName;
-
-    request.open('GET', requestURL);
+//get data from api
+    request.open('GET', requestURL); 
     request.responseType = 'text';
     request.send();
     request.onload = function () {
         data = request.response;
         data = JSON.parse(data);
     };
-parse.onclick = function () {
-    let mkp = data.data.market_cap_percentage,
-        size = Object.keys(mkp).length;
-        
+//load data to db by clicking button
+parse.onclick = function () {     
+    let mkp = data.data.market_cap_percentage; 
 
     for (let i in mkp) {
         
@@ -32,23 +31,33 @@ parse.onclick = function () {
 
 
 show.onclick = function () {
-    let table = document.createElement('table'),
+    let mkp = data.data.market_cap_percentage,
+        size = Object.keys(mkp).length,
+        table = document.createElement('table'),
         tbody = document.createElement('tbody'),
-        th = document.createElement('th'),
-        tr = document.createElement('tr'),
-        td = document.createElement('td');
-        //text = document.createTextNode()
-
+        tr = [],
+        th = [],
+        td = [];
+    // create a table 
+    for (let i=0; i<size; i++) {
+        tr[i] = document.createElement('tr');
+        th[i] = document.createElement('th');
+        td[i] = document.createElement('td');
     
-
-    for (let i in db) {
-        console.log(i);
-        tbody.appendChild(tr);
-        tr.appendChild(th);
-        th.innerHTML = criptoName;
-        tr.appendChild(td);
-        td.innerHTML = db[criptoName];
+        tr[i].appendChild(th[i]);
+        tr[i].appendChild(td[i]);
+        tbody.appendChild(tr[i]);
     }
     table.appendChild(tbody);
     document.getElementById('body').appendChild(table);
+    //fill the table
+    let i = 0;
+    for (let j in db) {
+        
+        if (db.hasOwnProperty(j)) {
+            th[i].innerHTML = j;
+            td[i].innerHTML = db[j];
+            i++;
+        }
+    }
 };
